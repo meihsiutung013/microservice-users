67d711a3cb33fe94f36bcfabf15f24278a00bdce
Mei Tung <meihsiu.tung013@comunidadunir.net>
2024-06-23 22:39:37 -0500

    add Dockerfile and railway profile

--- src/main/java/com/bewell/ms/users/MicroserviceUsersApplication.java
+++ src/main/java/com/bewell/ms/users/MicroserviceUsersApplication.java
@@ -2,11 +2,16 @@ package com.bewell.ms.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceUsersApplication {
 
     public static void main(String[] args) {
+        // Retrieve execution profile from environment variable. Otherwise, default profile is used.
+        String profile = System.getenv("PROFILE");
+        System.setProperty("spring.profiles.active", profile != null ? profile : "default");
         SpringApplication.run(MicroserviceUsersApplication.class, args);
     }
 

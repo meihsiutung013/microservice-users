7022a736c1e127165f20ad3b8d94d118d5ceb59d
Mei Tung <meihsiu.tung013@comunidadunir.net>
2024-06-28 15:04:43 -0500

    update hibernate ddl-auto mode

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO public.roles (id, created_at, updated_at, description, name) VALUES (1, '2024-06-23 08:08:19.072000', '2024-06-23 08:08:19.072000', 'Default user role', 'USER');
-INSERT INTO public.roles (id, created_at, updated_at, description, name) VALUES (2, '2024-06-23 08:08:19.120000', '2024-06-23 08:08:19.120000', 'Administrator role', 'ADMIN');
-INSERT INTO public.users (id, role_id, birth_date, created_at, updated_at, email, first_name, gender, last_name, password) VALUES (1, 2, null, '2024-06-23 08:08:19.308000', '2024-06-23 08:08:19.308000', 'admin@gmail.com', 'admin', null, 'admin', '$2a$10$k25ibqctum7KXp7h.iEeuOb7Ja2giHrSsnp5eTQyADyOaESFLE72O');
-INSERT INTO public.users (id, role_id, birth_date, created_at, updated_at, email, first_name, gender, last_name, password) VALUES (2, 1, null, '2024-06-23 08:08:19.489000', '2024-06-23 08:08:19.489000', 'user@gmail.com', 'user', null, 'user', '$2a$10$0Ghm92MlVqfM.kMcfmeiUuQe7ah.1trtSWygJZUP2Jzfq.L6Nn0oy');
+INSERT INTO public.roles (created_at, updated_at, description, name) VALUES ('2024-06-23 08:08:19.072000', '2024-06-23 08:08:19.072000', 'Default user role', 'USER');
+INSERT INTO public.roles (created_at, updated_at, description, name) VALUES ('2024-06-23 08:08:19.120000', '2024-06-23 08:08:19.120000', 'Administrator role', 'ADMIN');
+INSERT INTO public.users (role_id, birth_date, created_at, updated_at, email, first_name, gender, last_name, password) VALUES (2, null, '2024-06-23 08:08:19.308000', '2024-06-23 08:08:19.308000', 'admin@gmail.com', 'admin', null, 'admin', '$2a$10$k25ibqctum7KXp7h.iEeuOb7Ja2giHrSsnp5eTQyADyOaESFLE72O');
+INSERT INTO public.users (role_id, birth_date, created_at, updated_at, email, first_name, gender, last_name, password) VALUES (1, null, '2024-06-23 08:08:19.489000', '2024-06-23 08:08:19.489000', 'user@gmail.com', 'user', null, 'user', '$2a$10$0Ghm92MlVqfM.kMcfmeiUuQe7ah.1trtSWygJZUP2Jzfq.L6Nn0oy');
 
